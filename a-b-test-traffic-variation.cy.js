dfe28a7b53d72ca39ea2055f64f53beb1ad7a028
Rafael Prado <rafael.andrade.qa@gmail.com>
2024-11-19 14:26:21 -0300

    Update a-b-test-traffic-variation.cy.js

--- a-b-test-traffic-variation.cy.js
+++ a-b-test-traffic-variation.cy.js
@@ -1,34 +1,36 @@
 describe('Teste de CPF em Loop', () => {
-    const url = 'https://sua-url-aqui.com';
-  
-    let contadorCPFObrigatorio = 0;
-    let contadorCPFOpcional = 0;
-    let contadorDefault = 0;
-  
-    it('Realiza verificações de CPF em loop', () => {
-      Cypress._.times(100, () => {
-        cy.visit(url);
-  
-        cy.get('seu-seletor-aqui')
-          .then(($label) => {
+  const url = 'https://sua-url-aqui.com';
+
+  let contadorCPFObrigatorio = 0;
+  let contadorCPFOpcional = 0;
+  let contadorDefault = 0;
+
+  it('Realiza verificações de CPF em loop', () => {
+    Cypress._.times(100, () => {
+      cy.visit(url);
+
+      cy.get('body').then(($body) => {
+        if ($body.find('seu-seletor-aqui').length > 0) {
+          cy.get('seu-seletor-aqui').then(($label) => {
             const texto = $label.text();
-  
+
             if (texto === 'Digite seu CPF*') {
               contadorCPFObrigatorio += 1;
             } else if (texto === 'Digite seu CPF (opcional)') {
               contadorCPFOpcional += 1;
-            } else {
-              contadorDefault += 1;
             }
           });
-  
+        } else {
+          contadorDefault += 1;
+        }
       });
-    });
-  
-    after(() => {
-      cy.log(`Contador CPF Obrigatório: ${contadorCPFObrigatorio}`);
-      cy.log(`Contador CPF Opcional: ${contadorCPFOpcional}`);
-      cy.log(`Contador Default: ${contadorDefault}`);
+
     });
   });
-  
+
+  after(() => {
+    cy.log(`Contador CPF Obrigatório: ${contadorCPFObrigatorio}`);
+    cy.log(`Contador CPF Opcional: ${contadorCPFOpcional}`);
+    cy.log(`Contador Default: ${contadorDefault}`);
+  });
+});
